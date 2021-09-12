9bfb9332dc95f7dfdb4cdd955200275d301c5ff6
Aflahxrd <88531428+nexusNw@users.noreply.github.com>
2021-09-12 22:52:37 +0530

    Create git.js

---
+++ plugins/git.js
@@ -0,0 +1,29 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://imgur.com/a/ioP8jmW.jpg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Ruby bot by nabuhandbz*\n
+\n*owner number wa.me/918289836232*\n
+\n*CONTACT OWNER THROUGH social networks-https://www.instagram.com/im.naabu?r=nametag*\n
+\n*bot setting video : https://youtu.be/VR_ppqEl2q8*\n
+\n*githublink       https://github.com/nabuhandbz/ruby*\n
+\n*audio commads     https://github.com/nabuhandbz/ruby/tree/master/uploads*\n
+`}) 
+
+}));
