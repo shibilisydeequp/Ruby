aea82e386395dd6f744e35cb74126f26cf1bdbf7
nabuhandbz <90187970+nabuhandbz@users.noreply.github.com>
2021-09-13 00:30:58 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://imgur.com/a/ioP8jmW.jpg";
+  r_text[0] = "https://i.imgur.com/YqVlUfk.jpg";
     
     
     var i = Math.floor(1*Math.random())
